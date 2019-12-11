9350b90743fce0564a3e8ed80b95131def0a42d5
Bernard Allotey <benard.allotey4@gmail.com>
2019-12-11 15:33:57 -0600

    Finished Base Champion Class

--- ExperienceLevel/Models/Champion.cs
+++ ExperienceLevel/Models/Champion.cs
@@ -17,7 +17,11 @@ namespace ExperienceLevel.Models
         public string Title { get; set; }
         public string Blurb { get; set; }
         public InfoDt Info { get; set; }
-        
+        public ImageDt Image { get; set; }
+        public List<string> Tags { get; set; }
+        public string ParType { get; set; }
+        public StatsDt Stats { get; set; }
+
         private static List<Champion> RetrieveChamps()
         {
             return JsonConvert.DeserializeObject<List<Champion>>(WebIo.GetChampionsString(GameConstants.GameVersion));
@@ -51,6 +55,29 @@ namespace ExperienceLevel.Models
             public double W { get; set; }
             public double H { get; set; }
         }
-        
+
+        public struct StatsDt
+        {
+            public double Hp { get; set; }
+            public double HpPerLevel { get; set; }
+            public double Mp { get; set; }
+            public double MpPerLevel { get; set; }
+            public double MoveSpeed { get; set; }
+            public double Armor { get; set; }
+            public double ArmorPerLevel { get; set; }
+            public double SpellBlock { get; set; }
+            public double SpellBlockPerLevel { get; set; }
+            public double AttackRange { get; set; }
+            public double HpRegen { get; set; }
+            public double HpRegenPerLevel { get; set; }
+            public double MpRegen { get; set; }
+            public double MpRegenPerLevel { get; set; }
+            public double Crit { get; set; }
+            public double CritPerLevel { get; set; }
+            public double AttackDamage { get; set; }
+            public double AttackDamagePerLevel { get; set; }
+            public double AttackSpeedPerLevel { get; set; }
+            public double AttackSpeed { get; set; }
+        }
     }
 }
